3cd2b589e1ad8fa5228f4913649e1aaff3ec7dae
Kristoffer1995 <kristofferwj@hotmail.com>
2022-03-24 21:33:36 +0100

    MovementStrategies parted into sperate classes

---
+++ Galaga/MovementStrategy/NoMove.cs
@@ -0,0 +1,18 @@
+using Galaga.MovementStrategy;
+using DIKUArcade.Entities;
+using System.Collections.Generic;
+using System;
+using DIKUArcade.Math;
+using Galaga;
+
+namespace Galaga.MovementStrategy {
+    public class NoMove : IMovementStrategy {
+        public void MoveEnemy (Enemy enemy) {
+
+        }
+        public void MoveEnemies (EntityContainer<Enemy> enemies) {
+
+        }
+
+    }
+}
